88bec311295bfc97ba2722e1d201b860f709a5ea
Jeongdasilver <alal8272@gmail.com>
2018-07-31 15:10:42 +0900

    add GPS calculation

--- ChatPracticeDaeun/ChatPracticeAndroid/app/src/main/java/dasilver/jeong/chatpracticeandroid/ConnectActivity.java
+++ ChatPracticeDaeun/ChatPracticeAndroid/app/src/main/java/dasilver/jeong/chatpracticeandroid/ConnectActivity.java
@@ -19,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,7 +39,7 @@ public class ConnectActivity extends AppCompatActivity {
     private LocationManager locationManager;
     private boolean isGPSEnabled, isNetworkEnabled;
     private JSONObject data;
-    private double lat, lng;
+    private double lat, lng, distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +54,7 @@ public class ConnectActivity extends AppCompatActivity {
         socket = app.getSocket();
         socket.connect();
         socket.on("chat start", chatStart);
+        socket.on("connect fail", connectFail);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -61,12 +63,22 @@ public class ConnectActivity extends AppCompatActivity {
         // 네트워크 프로바이더 사용가능여부
         isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            //GPS 설정화면으로 이동
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            intent.addCategory(Intent.CATEGORY_DEFAULT);
-            startActivity(intent);
+
+            Toast.makeText(getApplicationContext(),"GPS를 켜세요",Toast.LENGTH_SHORT).show();
+
+            Runnable mMyTask = new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    intent.addCategory(Intent.CATEGORY_DEFAULT);
+                    //GPS 설정화면으로 이동
+                    startActivity(intent);
+                }
+            };
+            mHandler.postDelayed(mMyTask, 1500);
+
+
         }
 
         Log.d("Debug", "isGPSEnabled=" + isGPSEnabled);
@@ -78,7 +90,7 @@ public class ConnectActivity extends AppCompatActivity {
                 lat = location.getLatitude();
                 lng = location.getLongitude();
 
-                Log.d("Debug", "latitude: " + lat + ", longitude: " + lng);
+                Log.d("Location", "latitude: " + lat + ", longitude: " + lng);
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -105,21 +117,24 @@ public class ConnectActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        //사용자의 현재 위치 구하기
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-
-
+        //연결하기 버튼 클릭
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
+                    //server로 위도, 경도 전송
                     data.put("latitude",lat);
                     data.put("longitude",lng);
+                    Log.d("Debug","lat:"+lat+"lng:"+lng);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
                 socket.emit("login",data);
+                socket.emit("connect 100");
                 //startButton을 클릭할 경우 서버로 "login" emit
                 connectDialog();
                 //상대방과 연결시켜주는 다이얼로그를 띄워주는 메서드
@@ -151,23 +166,53 @@ public class ConnectActivity extends AppCompatActivity {
         }
     };
 
+
+    private Emitter.Listener connectFail = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            Log.d("Debug", "connectFail");
+            runOnUiThread(new Runnable() {
+                JSONObject data = (JSONObject) args[0];
+                @Override
+                public void run() {
+                    try {
+                        distance = data.getDouble("distance");
+                        Runnable mMyTask = new Runnable() {
+                            @Override
+                            public void run() {
+                                if(distance == 0.1) {
+                                    connectDialog.dismiss();
+                                    connectAgain100Dialog();
+                                }
+                                if(distance == 0.2) {
+                                    connectAgain100Dialog.dismiss();
+                                    connectAgain200Dialog();
+                                }
+                                if(distance == 0.4) {
+                                    connectAgain200Dialog.dismiss();
+                                    connectFailDialog();
+                                }
+                            }
+                        };
+                        mHandler.postDelayed(mMyTask, 4000);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+
+        }
+    };
+    // 100m 이내의 사람들을 찾아주는 Dialog
     private void connectDialog() {
         connectDialog = new ConnectDialog(ConnectActivity.this, cancelClickListener);
         connectDialog.setCancelable(true);
         connectDialog.getWindow().setGravity(Gravity.CENTER);
         connectDialog.show();
+        socket.on("connect fail", connectFail);
         Log.d("Debug", "connectDialog");
-
-        Runnable mMyTask = new Runnable() {
-            @Override
-            public void run() {
-                socket.emit("pop queue");
-                connectDialog.dismiss();
-                connectAgain100Dialog();
-            }
-        };
-        mHandler.postDelayed(mMyTask, 5000);//test 추후에 10000으로 바꿀 예정
-
     }
 
     private View.OnClickListener cancelClickListener = new View.OnClickListener() {
@@ -186,8 +231,6 @@ public class ConnectActivity extends AppCompatActivity {
         connectAgain100Dialog.getWindow().setGravity(Gravity.CENTER);
         connectAgain100Dialog.show();
         Log.d("Debug", "connectAgain100Dialog");
-
-
     }
 
     private View.OnClickListener againCancelClickListener = new View.OnClickListener() {
@@ -204,17 +247,8 @@ public class ConnectActivity extends AppCompatActivity {
         public void onClick(View view) {
             Log.d("Debug", "click button");
             connectAgain100Dialog.connectAgainProgressBar.setVisibility(View.VISIBLE);
+            socket.emit("connect 200");
 
-            Runnable mMyTask = new Runnable() {
-                @Override
-                public void run() {
-                    socket.emit("pop queue");
-                    connectAgain100Dialog.dismiss();
-                    connectAgain200Dialog();
-                    Log.d("Debug", "click button");
-                }
-            };
-            mHandler.postDelayed(mMyTask, 5000);//test 추후에 10000으로 바꿀 예정
         }
     };
 
@@ -223,6 +257,7 @@ public class ConnectActivity extends AppCompatActivity {
         connectAgain200Dialog.setCancelable(true);
         connectAgain200Dialog.getWindow().setGravity(Gravity.CENTER);
         connectAgain200Dialog.show();
+
         Log.d("Debug", "connectAgain200Dialog");
     }
 
@@ -237,20 +272,9 @@ public class ConnectActivity extends AppCompatActivity {
     private View.OnClickListener connectAgain400ClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
-
             Log.d("Debug", "click button");
             connectAgain200Dialog.connectAgainProgressBar.setVisibility(View.VISIBLE);
-            Runnable mMyTask = new Runnable() {
-                @Override
-                public void run() {
-                    socket.emit("pop queue");
-                    connectAgain200Dialog.dismiss();
-                    connectFailDialog();
-                    Log.d("Debug", "click button");
-                }
-            };
-            mHandler.postDelayed(mMyTask, 5000);//test 추후에 10000으로 바꿀 예정
+            socket.emit("connect 400");
         }
 
 
