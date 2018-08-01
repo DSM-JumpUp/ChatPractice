1cc0139c4e91951ace910962169ae268bcdc914c
Sungyong Son <space010127@gmail.com>
2018-08-01 09:26:55 +0900

    Modified pacakge.json

--- SeungYong/chat/LocationRoomIndex.js
+++ SeungYong/chat/LocationRoomIndex.js
@@ -23,7 +23,6 @@ const toDistance = function toDistanceFromCoordinate(lat1, lat2, lng1, lng2) {
     return Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lng1 - lng2, 2)) / RAD;
 }
 
-
 var findPeerForLoneSocket = function (socket) {
     // this is place for possibly some extensive logic
     // which can involve preventing two people pairing multiple times
