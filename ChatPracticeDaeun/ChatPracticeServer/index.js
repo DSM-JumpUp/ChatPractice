2ac3debf79d41bd579c8ba2f7254e5f54c4fb33a
Jeongdasilver <alal8272@gmail.com>
2018-08-01 10:01:17 +0900

    add explanation

--- ChatPracticeDaeun/ChatPracticeServer/index.js
+++ ChatPracticeDaeun/ChatPracticeServer/index.js
@@ -6,108 +6,108 @@ const server = app.listen(3000, () => {
 });
 const io = SocketIo(server);
 
-var queue = [];    // list of sockets waiting for peers
-var rooms = {};    // map socket.id => room
-var names = {};    // map socket.id => name
-var allUsers = {}; // map socket.id => socket
-var locations = {};
-var lat1 = 0;
-var lng1 = 0;
-var lat2 = 0;
-var lng2 = 0;
+var queue = [];    //상대방과의 연결을 위한 대기열
+var rooms = {};    //방 목록
+var allUsers = {}; //모든 socket의 id 목록
+var locations = {}; //모든 socket의 위치 목록
+var lat1 = 0; //나의 위도
+var lng1 = 0; //나의 경도
+var lat2 = 0; //상대방의 위도
+var lng2 = 0; //상대방의 경도
 var i=0;
 
-io.on('connection', function (socket) {
+io.on('connection', function (socket) { //socket 연결을 기다리는 함수
+
     console.log('User '+socket.id + ' connected');
+    
     socket.on('login', function (data) {
         allUsers[socket.id] = socket;
         locations[socket.id] = data;
-        //my location
+        //내 위치 받아오기
         lat1 = data.latitude;
         lng1 = data.longitude;
         console.log("my location : "+lat1, lng1);
-        // now check if sb is in queue
     });
-    socket.on('connect 100', function() {
+    socket.on('connect 100', function() { //100m 이내 사람들 연결
         console.log("connect 100");
         findPeerForLoneSocket(socket,0.1);
     });
 
-    socket.on('connect 200', function() {
+    socket.on('connect 200', function() { //200m 이내 사람들 연결
         console.log("connect 200");        
         findPeerForLoneSocket(socket,0.2);
     });
 
-    socket.on('connect 400', function() {
+    socket.on('connect 400', function() { //400m 이내 사람들 연결
         console.log("connect 400");        
         findPeerForLoneSocket(socket,0.4);
     });
 
-    socket.on('pop queue', function() {
+    socket.on('pop queue', function() { //대기열에서 자기 자신을 pop
         queue.splice(queue.indexOf(socket), 1);
     });
-    socket.on('message', function (data) {
+    socket.on('message', function (data) { //message를 받았을 경우
         var room = rooms[socket.id];
         console.log(data);
-        socket.broadcast.to(room).emit('message', data);
+        socket.broadcast.to(room).emit('message', data); //자신을 제외한 나머지 유저에게 message전송
     });
-    socket.on('leave room', function () {
+    socket.on('leave room', function () { //방을 나갔을 경우
         var room = rooms[socket.id];
         console.log('leave room');
-        socket.broadcast.to(room).emit('chat end');
+        socket.broadcast.to(room).emit('chat end'); //자신을 제외한 나머지 유저에게 chat end전송
     });
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function () { //socket연결이 끊겼을 경우
         var room = rooms[socket.id];
-        socket.broadcast.to(room).emit('chat end');
-        queue.splice(queue.indexOf(socket), 1);
+        socket.broadcast.to(room).emit('chat end'); //자신을 제외한 나머지 유저에게 chat end전송
+        queue.splice(queue.indexOf(socket), 1); //자신을 대기열에서 pop
     });
 });
 
 //상대방을 찾는 함수
 var findPeerForLoneSocket = function(socket, distance) {
     console.log("queue.length : "+queue.length);
-    if (queue.length > 0) {
-        for(i=0; i<10;i++) {
-            var peer = queue.pop();
-            if(peer == socket) {
-                findPeerForLoneSocket(socket, distance);
-            }
-            lat2 = locations[peer.id].latitude;
-            lng2 = locations[peer.id].longitude;
-            console.log("peer location"+lat2, lng2);
 
+    if (queue.length > 0) { //대기열의 크기가 0보다 클 때
+        for(i=0; i<10;i++) { //상대방을 10번 찾음
+            var peer = queue.pop(); //상대방을 대기열에서 빼기
+            if(peer == socket) { //대기열에서 빼낸 상대방이 자기 자신일 경우
+                findPeerForLoneSocket(socket, distance); //다시 함수 호출
+            }
+            lat2 = locations[peer.id].latitude; //상대방의 위도
+            lng2 = locations[peer.id].longitude; //상대방의 경
+            도
+            console.log("peer location"+lat2, lng2);
             console.log("calculateDistance : "+calculateDistance(lat1,lng1,lat2,lng2));
 
-            if(calculateDistance(lat1,lng1,lat2,lng2) <= distance) {
+            if(calculateDistance(lat1,lng1,lat2,lng2) <= distance) { //상대방과의 거리가 자신이 찾고자 하는 거리보다 작을 경우
                 console.log("success");
-                var room = socket.id + '#' + peer.id;
+                var room = socket.id + '#' + peer.id; //방 만들기
                 console.log(room);
-                // join them both
-                peer.join(room);
+                //방에 입장
+                peer.join(room); 
                 socket.join(room);
-                // register rooms to their names
+                // rooms에 방 저장
                 rooms[peer.id] = room;
                 rooms[socket.id] = room;
-                // exchange names between the two of them and start the chat
+                // 상대방과 자신에게 chat start전송
                 peer.emit('chat start', {'room':room});
                 socket.emit('chat start', {'room':room});
                 break;
-            } else {
+            } else { //상대방과의 거리가 자신이 찾고자 하는 거리보다 클 경우
                 console.log("push queue");
-                queue.push(peer);
+                queue.push(peer); //상대방을 다시 대기열에 push
             }
         }
-        socket.emit('connect fail', {'distance': distance});
-    } else {
-        // queue is empty, add our lone socket
+        socket.emit('connect fail', {'distance': distance}); //10번동안 상대방을 찾지 못할 경우 connect fail전송
+    } else { //대기열의 크기가 1보다 작을 경우
         console.log('push queue');
-        queue.push(socket);
+        queue.push(socket); //대기열에 자기 자신을 넣음
         console.log("queue" + queue);
-        socket.emit('connect fail', {'distance': distance});
+        socket.emit('connect fail', {'distance': distance}); //connect fail전송
     }
 }
 
-//두 좌표 간의 거리 구하는 함수
+//두 좌표 간의 거리 구하는 함수(단위:km)
 function calculateDistance(lat1, lon1, lat2, lon2) {
     console.log("calculatedistance("+lat1,lon1,lat2,lon2+")");
     var R = 6371; // km
