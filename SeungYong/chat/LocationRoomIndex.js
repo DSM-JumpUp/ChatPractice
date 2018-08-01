9f2876bc7fbfd736b145d1b84d29a806db2e71cd
Sungyong Son <space010127@gmail.com>
2018-08-01 11:51:28 +0900

    Fix Location Random Chatting Server

--- SeungYong/chat/LocationRoomIndex.js
+++ SeungYong/chat/LocationRoomIndex.js
@@ -12,50 +12,49 @@ var queue = []; // list of sockets waiting for peers
 var rooms = {}; // map socket.id => room
 var allUsers = {}; // map socket.id => socket
 
-var locationLat = {};
-var locationLng = {};
+var locationLat = [];
+var locationLng = [];
 
 var isDistatnce = [];
 
 const toDistance = function toDistanceFromCoordinate(lat1, lat2, lng1, lng2) {
     const RAD = 0.000008998719243599958;
-    console.log(Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lng1 - lng2, 2)) / RAD);
+    console.log(lat1 + " " + lat2 + " " + lng1 + " " + lng2);
+    console.log("result " + Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lng1 - lng2, 2)) / RAD)
     return Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lng1 - lng2, 2)) / RAD;
 }
 
 var findPeerForLoneSocket = function (socket) {
-    // this is place for possibly some extensive logic
-    // which can involve preventing two people pairing multiple times
-    console.log(queue.length);
     if (queue.length > 0) {
-        // somebody is in queue, pair them!
         for (var peer in queue) {
-            if (toDistance(locationLat[socket.id], locationLng[socket.id], locationLat[peer.id], locationLng[peer.id]) < 100.0) {
-                queue.splice(queue.indexOf(peer.id), 1);
-                locationLat.splice(locationLat.indexOf(peer.id), 1);
-                locationLng.splice(locationLng.indexOf(peer.id), 1);
+            console.log(queue[peer]);
+            if (toDistance(locationLat[socket.id], locationLat[queue[peer].id], locationLng[socket.id], locationLng[queue[peer].id]) < 100.0) {
+                locationLat.splice(locationLat.indexOf(queue[peer].id), 1);
+                locationLng.splice(locationLng.indexOf(queue[peer].id), 1);
                 locationLat.splice(locationLat.indexOf(socket.id), 1);
                 locationLng.splice(locationLng.indexOf(socket.id), 1);
-                var room = socket.id + '#' + peer.id;
+                var room = socket.id + '#' + queue[peer].id;
                 console.log(room);
                 // join them both
-                peer.join(room);
+                queue[peer].join(room);
                 socket.join(room);
                 // register rooms to their names
-                rooms[peer.id] = room;
+                rooms[queue[peer].id] = room;
                 rooms[socket.id] = room;
                 // exchange names between the two of them and start the chat
-                peer.emit('chat start', {
+                queue[peer].emit('chat start', {
                     'room': room
                 });
                 socket.emit('chat start', {
                     'room': room
                 });
                 isDistatnce[socket.id] = true;
-                isDistatnce[peer.id] = true
+                isDistatnce.splice(queue.indexOf(queue[peer].id), 1);
+                queue.splice(queue.indexOf(queue[peer].id), 1);
                 break;
             }
-            if (!isDistatnce) queue.push(socket)
+            if (!isDistatnce[socket.id]) queue.push(socket);
+            else isDistatnce.splice(queue.indexOf(socket.id), 1);
         }
     } else {
         // queue is empty, add our lone socket
@@ -94,12 +93,14 @@ io.on('connection', function (socket) {
         findPeerForLoneSocket(allUsers[peerID]);
         findPeerForLoneSocket(socket);
     });
+    /*
     socket.on('disconnect', function () {
         var room = rooms[socket.id];
-        socket.broadcast.to(room).emit('chat end');
-        var peerID = room.split('#');
+        //socket.broadcast.to(room).emit('chat end');
         peerID = peerID[0] === socket.id ? peerID[1] : peerID[0];
-        // current socket left, add the other one to the queue
-        //findPeerForLoneSocket(allUsers[peerID]);
+        queue.splice(queue.indexOf(peer.id), 1);
+        queue.splice(queue.indexOf(socket.id), 1);
     });
+    */
+   socket.on('disconnect', function () {});
 });
